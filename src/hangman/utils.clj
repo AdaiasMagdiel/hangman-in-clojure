baf550a3fb348489e03cbf1cdd7d968cbef11ac9
Adaías Magdiel <adaiasmagdiell@gmail.com>
2024-03-09 14:46:23 -0300

    style(utils): Improve code formatting and add utility functions - Refactor code to use consistent formatting and indentation - Add functions for generating secret word, showing strings, checking   if a collection contains a value, and checking if a string contains a   substring.

--- src/hangman/utils.clj
+++ src/hangman/utils.clj
@@ -1,11 +1,25 @@
 (ns hangman.utils
   (:gen-class)
-  (:require [clojure.java.io :as io]))
+  (:require [clojure.java.io :as io])
+  (:require [clojure.string :as str]))
+
+(defn generate-secret-word [string]
+	(repeat (count string) \_))
+
+(defn show-string
+	([string] (str/join string))
+	([string join] (str/join join string)))
+
+(defn list-contains? [collection value]
+  (some #(= value %) collection))
+
+(defn istr-contains? [s value]
+  (not (nil? (str/index-of s (str/lower-case value)))))
 
 (defn choice [collection]
   (let [idx (rand-int (count collection))]
   (let [item (nth collection idx)]
-  item)))
+    item)))
 
 (defn get-lines [path]
   (with-open [reader (io/reader path)]
